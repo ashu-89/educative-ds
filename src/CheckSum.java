c29e92d5c8dd8ad56ae11065e64323f6c9b1582c
Ashutosh Pathak <ashu.p89@gmail.com>
2023-06-25 12:08:49 +0530

    optimized 1 - assume array is sorted

--- src/CheckSum.java
+++ src/CheckSum.java
@@ -1,5 +1,7 @@
 class CheckSum
 {
+   static int count = 0;
+
     public static int[] findSum(int[] arr, int n)
     {
         int[] result = new int[2];
@@ -9,8 +11,15 @@ class CheckSum
                 if(arr[i] + arr[j] == n){
                     result[0] = arr[i];
                     result[1] = arr[j];
+                    count ++;
                     return  result;
+                } else if(arr[i]+arr[j] > n){
+                    count++;
+                    break;
+                } else {
+                    count ++;
                 }
+
             }
         }
 
@@ -22,11 +31,19 @@ class CheckSum
 
     public static void main(String[] args) {
 
+        int arr[] = {5,6,7,9,10};
+        int n = 13;
 
-        int arr[] = {5,6,7,8,9,10};
-        int n = 20;
 
         int[] result = findSum(arr,n);
+
+        if(result.length !=2 ){
+            System.out.println("no 2 numbers add up to " + n);
+        } else {
+            System.out.println("the following 2 numbers add upto " + n);
+        }
+
+        System.out.println("no of steps executed " + count);
 
         for(int i=0; i<result.length; i++){
             System.out.println(result[i]);
