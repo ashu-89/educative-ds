39abb0707dae1709ac47668f5bcb1b497e4ee4fc
Ashutosh Pathak <ashu.p89@gmail.com>
2023-06-25 14:00:08 +0530

    working

--- src/ProductArray.java
+++ src/ProductArray.java
@@ -4,24 +4,21 @@ class ProductArray
     {
         int [] result = new int[arr.length];
 
-        for(int i = 0; i < arr.length; i++){
-
-            result[i] = 1;
-
-            for(int j = 0; j < arr.length; j++){
-
-                if(i==j){
-
-                }
-                else{
-                    result[i] *= arr[j];
-                }
-
-            }
+        //in the first traversal, at each index i, we'll store in result of i, product of all nos to the left of i
+        int temp = 1;
+        for(int i = 0; i<arr.length; i++){
+            result[i]=temp;
+            temp *= arr[i];
         }
 
+        //in the second traversal, store product of all nos to the right
+        temp = 1;
+        for(int i = arr.length-1; i >= 0; i--){
+            result[i] *= temp;
+            temp *= arr[i];
 
-        // write your code here
+        }
+
 
         return result;
     }
